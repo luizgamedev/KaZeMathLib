c90688a42ae51a98fdd8647dafc10bc070fe730d
Luiz Santos <santos.uff@gmail.com>
2022-01-01 17:45:42 +0000

    Adding the New Matrix Files and Test Files
    Adding the initial Matrix Files

---
+++ MathLib/Tests/Matrix/Matrix2x2Tests.cpp
@@ -0,0 +1,41 @@
+//
+// Created by Luiz on 31/12/2021.
+//
+
+#include "../../../catch.hpp"
+#include "../../Matrix/Matrix2x2.cpp"
+
+using namespace KMath;
+
+TEST_CASE("2x2 Matrix Creation and Access", "[Matrix2x2]")
+{
+    Matrix2x2<float> myMatrix();
+
+
+}
+
+TEST_CASE("2x2 Matrix Add and Subtraction", "[Matrix2x2]")
+{
+
+}
+
+TEST_CASE("2x2 Matrix Multiplication", "[Matrix2x2]")
+{
+
+}
+
+TEST_CASE("2x2 Matrix Identity Cases", "[Matrix2x2]")
+{
+
+}
+
+TEST_CASE("2x2 Matrix Determinant", "[Matrix2x2]")
+{
+
+}
+
+TEST_CASE("2x2 Matrix Inverse", "[Matrix2x2]")
+{
+
+}
+
---
+++ MathLib/Tests/Matrix/Matrix3x3Tests.cpp
@@ -0,0 +1,36 @@
+//
+// Created by Luiz on 31/12/2021.
+//
+
+#include "../../../catch.hpp"
+#include "../../Matrix/Matrix3x3.cpp"
+
+TEST_CASE("3x3 Matrix Creation and Access", "[Matrix3x3]")
+{
+
+}
+
+TEST_CASE("3x3 Matrix Add and Subtraction", "[Matrix3x3]")
+{
+
+}
+
+TEST_CASE("3x3 Matrix Multiplication", "[Matrix3x3]")
+{
+
+}
+
+TEST_CASE("3x3 Matrix Identity Cases", "[Matrix3x3]")
+{
+
+}
+
+TEST_CASE("3x3 Matrix Determinant", "[Matrix3x3]")
+{
+
+}
+
+TEST_CASE("3x3 Matrix Inverse", "[Matrix3x3]")
+{
+
+}
